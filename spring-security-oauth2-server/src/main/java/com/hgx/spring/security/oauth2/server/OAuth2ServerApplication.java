a4e6ba45d8288da36774de069cbff77cb453c824
小新哥 <403713133@qq.com>
2020-07-02 16:49:35 +0800

    first commit

---
+++ spring-security-oauth2-server/src/main/java/com/hgx/spring/security/oauth2/server/OAuth2ServerApplication.java
@@ -0,0 +1,16 @@
+package com.hgx.spring.security.oauth2.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author hgx
+ * @date 2020/7/2 15:44
+ * @Description
+ */
+@SpringBootApplication
+public class OAuth2ServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OAuth2ServerApplication.class);
+    }
+}
