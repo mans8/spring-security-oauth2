75aee9fa9aa2a532b03d842ebe106d63cc01963e
小新哥 <403713133@qq.com>
2020-07-02 23:52:18 +0800

    添加mysql数据源

--- spring-security-oauth2-server/src/main/java/com/hgx/spring/security/oauth2/server/configure/AuthorizationServerConfiguration.java
+++ spring-security-oauth2-server/src/main/java/com/hgx/spring/security/oauth2/server/configure/AuthorizationServerConfiguration.java
@@ -1,12 +1,29 @@
 package com.hgx.spring.security.oauth2.server.configure;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.ClientDetails;
+import org.springframework.security.oauth2.provider.ClientDetailsService;
+import org.springframework.security.oauth2.provider.ClientRegistrationException;
+import org.springframework.security.oauth2.provider.client.JdbcClientDetailsService;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
+
+import javax.sql.DataSource;
 
 /**
  * @author hgx
@@ -20,14 +37,43 @@ public class AuthorizationServerConfiguration extends AuthorizationServerConfigu
     @Autowired
     private BCryptPasswordEncoder passwordEncoder;
 
+    @Autowired
+    DataSourceProperties dataSourceProperties;
+
+    //配置数据源
+    @Bean
+    @Primary
+    @ConfigurationProperties(prefix = "spring.datasource")
+    public DataSource dataSource() {
+        HikariConfig hikariConfig=new HikariConfig();
+        hikariConfig.setJdbcUrl("jdbc:mysql://192.168.1.54:3306/oauth2?useUnicode=true&characterEncoding=utf-8&useSSL=false");
+        hikariConfig.setUsername("root");
+        hikariConfig.setPassword("123456");
+        hikariConfig.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        HikariDataSource hikariDataSource=new HikariDataSource(hikariConfig);
+        return hikariDataSource;
+        //return DataSourceBuilder.create().build();
+    }
+
+    //保存token到数据库
+    @Bean
+    public TokenStore tokenStore() {
+        return new JdbcTokenStore(dataSource());
+    }
+
+    //从数据库中读取客户端配置
+    @Bean
+    public ClientDetailsService jdbcClientDetailsService() {
+        return new JdbcClientDetailsService(dataSource());
+    }
+
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        endpoints.tokenStore(tokenStore());
+    }
+
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-        clients.inMemory()
-                .withClient("client")
-                .secret(passwordEncoder.encode("secret"))
-                .authorizedGrantTypes("authorization_code")
-                .scopes("app")
-                .redirectUris("https://www.baidu.com");
-        System.out.println(clients.toString());
+        clients.withClientDetails(jdbcClientDetailsService());
     }
 }
